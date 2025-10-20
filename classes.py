1359bc8e808d091c5c15dbd7d833d83babd75618
luananicolau <luananicolauln@gmail.com>
2025-10-20 14:53:48 -0300

    atualiza projeto

---
+++ classes.py
@@ -0,0 +1,38 @@
+"""
+POO - classes
+
+classes nada mais são do que modelos dos objetos do mundo real sendo representados computacionalmente
+
+classes podem conter:
+- atributos(represenatm as caracteristicas do objeto, ou seja, conseguimos representar os estados de um objeto
+ex: cores, voltagem, luminosidade etc
+
+- metodos (funções) -> representam os comportamentos do onjetos. Ou seja, as ações que este objeto pode realizar no seu sistema.
+ex: ligar e desligar
+
+Em Python, para definir uma classe utilizamos a palavra reservada class
+
+OBS: utilizamos a palavra 'pass' em Python quando temos um bloco de código que ainda não está implementado
+
+OBS: iniciais maiúsculas
+
+OBS: quando estamos planejando um software e definimos quais classes teremos que ter no sistema, chamamos estes
+objetos que serão mapeados para classes de entidade.
+
+
+"""
+
+class lampada:
+    pass
+
+class ContaCorrente:
+    pass
+
+class Usuario:
+    pass
+
+lamp = lampada()
+
+print(type(lamp))
+
+
---
+++ poo.py
@@ -0,0 +1,29 @@
+"""
+Programação Orientada a Objetos
+
+- POO é um paradigma de programação que utiliza um mapeamento
+de objetos do mundo real para modelos computacionais
+
+- paradigma de programação é a forma/ metodologia utilizada para pensar/ desenvolver sistemas
+
+Principais elementos da orientação a Objetos
+
+-Classe -> Modelo do Objeto do mundo real sendo representado computacionalmente;
+-Atributo -> Características do objeto
+-Método -> Comportamento do objeto (funções)
+-Construtor -> Método especial utilizado para criar objetos;
+-Objeto -> Instância de classe
+"""
+
+numero = 10
+print(numero)
+print(type(numero))
+
+nome = 'luana'
+
+class Produto:
+    pass
+
+ps4 = Produto()
+print(ps4)
+print(type(ps4))
